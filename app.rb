1deb5507879298652c4ef1e989129d8c0a964ae3
YumaYX <86939129+YumaYX@users.noreply.github.com>
2024-01-31 23:02:46 +0900

    update

--- app.rb
+++ app.rb
@@ -1,23 +1,22 @@
 #!/usr/bin/env ruby
 
+def puts_plus(str)
+  puts str
+  puts
+end
+
 Dir.chdir('codes')
 rb_scripts_names =  Dir.glob("*.rb").map { |element| element.gsub(/codes\//,'') }
 heads = rb_scripts_names.map{|element|element.split('_').first}.uniq.sort
 
-puts "# Ruby Quick Reference"
-puts
-
-puts "## Contents"
-puts
-
+puts_plus "# Ruby Quick Reference"
+puts_plus "## Contents"
 
 heads.each do |element|
-  puts '### '+ element.capitalize
-  puts
-  puts '<ul>'
+  puts_plus '### '+ element.capitalize
+  puts_plus '<ul>'
   Dir.glob(element+'*.rb').each do |file|
     puts "<li><a href=\"#{file.gsub(/\.rb$/,'')}.html\">#{File.basename(file)}</a></li>"
   end
-  puts '</ul>'
-  puts
+  puts_plus '</ul>'
 end
