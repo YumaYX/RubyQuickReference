ebe813f966f77b6513f9882403ffd927d04d274e
YumaYX <86939129+YumaYX@users.noreply.github.com>
2024-04-11 21:19:39 +0900

    update

--- codes/csv_read_with_read_bom.rb
+++ codes/csv_read_with_read_bom.rb
@@ -1,2 +1,6 @@
 require 'csv'
-CSV.read('file.csv', encoding: 'BOM|UTF-8', headers: true).map(&:to_h)
+if RUBY_VERSION >= "3.0"
+  CSV.read('file.csv', encoding: "BOM|UTF-8", headers: true).map(&:to_h)
+else
+  CSV.read('file.csv', "r:BOM|UTF-8", headers: true).map(&:to_h)
+end
--- codes/csv_read_with_read_bom_hash.rb
+++ codes/csv_read_with_read_bom_hash.rb
@@ -1,9 +1,16 @@
 require 'csv'
 hash = {}
-csv_data = CSV.read('file.csv', encoding: 'BOM|UTF-8', headers: true)
+
+csv_data = if RUBY_VERSION >= "3.0"
+  CSV.read('file.csv', encoding: "BOM|UTF-8", headers: true)
+else
+  CSV.read('file.csv', "r:BOM|UTF-8", headers: true)
+end
+
 csv_data.each do |row|
   header_key = row.headers.first
   key = row[header_key]
   hash[key] = row.to_h
 end
+
 hash
