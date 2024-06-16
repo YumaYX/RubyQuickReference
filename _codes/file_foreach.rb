5ec6e1563d123010ce7fdb7dbdbd8ff9a6a4f53e
YumaYX <86939129+YumaYX@users.noreply.github.com>
2024-06-17 00:20:50 +0900

    update

--- _codes/file_foreach.rb
+++ _codes/file_foreach.rb
@@ -1,5 +1,5 @@
-File.foreach('file_each_line_b.txt') do |line1|
-  File.foreach('file_each_line_a.txt') do |line2|
+File.foreach('file_foreach_b.txt') do |line1|
+  File.foreach('file_foreach_a.txt') do |line2|
     if line1.eql?(line2)
       puts("MATCHED: #{line1}")
       break
