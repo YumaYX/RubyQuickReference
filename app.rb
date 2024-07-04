1bf5e6ced103a8a3a8ec0d44a77739ae62ede012
YumaYX <86939129+YumaYX@users.noreply.github.com>
2024-07-04 23:06:00 +0900

    update

--- app.rb
+++ app.rb
@@ -10,12 +10,12 @@ Dir.chdir('_codes')
 rb_scripts_names = Dir.glob('*.rb').map { |element| element.gsub(%r{_codes/}, '') }
 heads = rb_scripts_names.map { |element| element.split('_').first }.uniq.sort
 
-puts_plus '# Official Reference Manual'
-puts_plus '- [Link](https://docs.ruby-lang.org/ja/latest/doc/index.html)'
 puts_plus '# Contents'
+puts_plus '## Official Reference Manual'
+puts_plus '- [Link](https://docs.ruby-lang.org/ja/latest/doc/index.html)'
 
 heads.each do |element|
-  puts_plus "## #{element.capitalize}"
+  puts_plus "### #{element.capitalize}"
   puts_plus '<ul>'
   Dir.glob("#{element}*.rb").each do |file|
     puts "<li><a href=\"#{file.gsub(/\.rb$/, '')}.html\">#{file}</a></li>"
