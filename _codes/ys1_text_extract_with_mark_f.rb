c60f982d1620017cf37c215547c9fb585544bbd9
YumaYX <86939129+YumaYX@users.noreply.github.com>
2024-07-14 09:44:02 +0900

    update

--- _codes/ys1_text_extract_with_mark_f.rb
+++ _codes/ys1_text_extract_with_mark_f.rb
@@ -1,4 +1,4 @@
 require "ys1"
 
-YS1::Text.extract_with_mark_f("ys1_text_extract_with_mark_f.txt", /^header/)
-
+pac = YS1::Text.extract_with_mark_f("ys1_text_extract_with_mark_f.txt", /^header/)
+pac.class
